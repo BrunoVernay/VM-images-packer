61cb18e0e23e07ea04487b58d4c6070fa44fda24
BrunoVernay <BrunoVern.a@gmail.com>
2016-09-21 18:40:33 +0200

    DNS problem solved
    Change-Id: Ic9325cab62f04492064afb1e2f06c2aa8aeaca2e

---
+++ provisioners/shell/mininet.sh
@@ -0,0 +1,13 @@
+#
+# Install Mininet on Fedora
+# http://mininet.org/download/#option-2-native-installation-from-source
+#
+# WARNING:  Work in progress !
+
+sudo dnf install git
+git clone https://github.com/mininet/mininet.git
+cd mininet
+git checkout -b 2.2.1 2.2.1
+
+
+
--- provisioners/shell/openstack.sh
+++ provisioners/shell/openstack.sh
@@ -6,6 +6,11 @@ sudo yum -y remove NetworkManager firewalld
 
 sudo systemctl enable network
 
+#for i in {1..50}; do ping -c1 www.google.com &> /dev/null && break; done
+/usr/sbin/ip a
+cat /etc/resolv.conf
+#cat -lh /etc/
+
 # Must be done in 2 steps: one is the repo, the second is the package
 sudo yum install -y centos-release-openstack-mitaka 
 sudo yum update -y
